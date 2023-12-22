a4da11fbcb890f9702de6a628974e16412884a88
Levan Loladze <levan.loladzew@gmail.com>
2023-12-22 12:49:38 +0400

    first commit

---
+++ Gestures/Gestures/GesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesturesApp.swift
+//  Gestures
+//
+//  Created by Levan Loladze on 22.12.23.
+//
+
+import SwiftUI
+
+@main
+struct GesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
